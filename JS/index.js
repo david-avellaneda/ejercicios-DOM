fdda9fa8a08ea88326fab5668fe60186e7902377
David Avellaneda <davidavellanedagit@gmail.com>
2021-07-23 22:42:51 -0500

    Ejercicio del botón de reproducir sonido

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,9 @@
 import menuHamburguesa from "./menu_hamburguesa.js";
 import { digitalClock } from "./digital_clock.js";
+import musicBotton from "./music_botton.js";
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
     menuHamburguesa();
     digitalClock("#realTimeClock", "#activateWatch", "#deactivateWatch");
+    musicBotton("#activateMusic", "#deactivateMusic");
 });
