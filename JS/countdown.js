cd41023851cce5bb254c13a457d6061e0de31bea
David Avellaneda <davidavellanedagit@gmail.com>
2021-07-26 21:48:00 -0500

    Ejercicio del cuenta regresiva terminado

--- JS/countdown.js
+++ JS/countdown.js
@@ -1,51 +1,37 @@
-const days = document.querySelector(".days"),
-    hours = document.querySelector(".hours"),
-    minutes = document.querySelector(".minutes"),
-    seconds = document.querySelector(".seconds");
-
-let timeLeft = {
-    d:0, h:0, m:0, s:0,
-};
-let totalSeconds = 0;
-export default function init() {
-    totalSeconds = Math.round((new Date("12.24.2021") - new Date()) / 1000); // 100 milisegundos = 1s
-    setTimeLeft();
-    let interval = setInterval(() => {
-        if(totalSeconds < 0 ){
-            clearInterval(interval);
+const $countdown = document.querySelector(".countdown"),
+    $days = document.getElementById("days"),
+    $hours = document.getElementById("hours"),
+    $minutes = document.getElementById("minutes"),
+    $seconds = document.getElementById("seconds"),
+    $timeInDays = document.querySelector(".timeInDays"),
+    $timeInHours = document.querySelector(".timeInHours"),
+    $timeInMinutes = document.querySelector(".timeInMinutes"),
+    $timeInSeconds = document.querySelector(".timeInSeconds");
+export default function countdown(dateLimit) {
+    const countdownDate = new Date(dateLimit).getTime(); // getTime obtenemos la fecha en milisegundos para hacer más fácil los cálculos
+    let countdownCounter = setInterval(() => {
+        let now = new Date().getTime(), // La fecha de hoy, tiempo real en milisegundos
+            timeLimit = countdownDate - now;
+        let days = Math.floor(timeLimit / (1000 * 60 * 60 * 24));
+            $days.innerHTML = `<h3>${days}</h3>`;
+            if(days === 1) $timeInDays.innerHTML = `<h3>Día</h3>`;
+            if(days > 1 || days === 0) $timeInDays.innerHTML = `<h3>Días</h3>`;
+        let hours = ("0" + Math.floor((timeLimit % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))).slice(-2); // EL NÚMERO "0" QUE ESTÁ AL LADO ES PARA QUE CUANDO LLEGUE A LA HORA 9 IMPRIMA 09 CON .slice(-2) SIRVE PARA RECORTAR, COMO EL VALOR NEGATIVO VA HACIA ATRÁS ES DECIR QUE VA A TOMAR DE DERECHA A IZQUIERDA, DEL ÚLTIMO NÚMERO EN LA DERECHA HACIA EL SEGUNDO HACIA LA IZQUIERDA, Y ASÍ NO SALDRÁ EL 0 CUNADO SEAN LAS 11 12 13 ETC, entonces cuando sea la hora 9 solo imprimirá 9 y el 0 se va a añadir gracias al slice
+            $hours.innerHTML = `<h3>${hours}</h3>`;
+            if(hours == 1) $timeInHours.innerHTML = `<h3>Hora</h3>`;
+            if(hours > 1 || hours == 0) $timeInHours.innerHTML = `<h3>Horas</h3>`;
+        let minutes = ("0" + Math.floor((timeLimit % (1000 * 60 * 60)) / (1000 * 60))).slice(-2);
+            $minutes.innerHTML = `<h3>${minutes}</h3>`;
+            if(minutes == 1) $timeInMinutes.innerHTML = `<h3>Minuto</h3>`;
+            if(minutes > 1 || minutes == 0) $timeInMinutes.innerHTML = `<h3>Minutos</h3>`;
+        let seconds = ("0" + Math.floor((timeLimit % (1000 * 60)) / 1000)).slice(-2);
+            $seconds.innerHTML = `<h3>${seconds}</h3>`;
+            if(seconds == 1) $timeInSeconds.innerHTML = `<h3>Segundo</h3>`;
+            if(seconds > 1 || seconds == 0) $timeInSeconds.innerHTML = `<h3>Segundos</h3>`;
+        console.log(countdownDate, now, timeLimit); // limitTime es la diferencia de tiempo, como la distancia en milisegundos entre fechas
+        if(timeLimit < 0){
+            clearInterval(countdownCounter);
+            $countdown.innerHTML = `<h3>Segu</h3>`
         };
-        countTime();
-        //console.log(timeLeft);
-    }, 1000);
+    });
 };
-function countTime() {
-    if(totalSeconds > 0){
-        --timeLeft.s; // El operador de decremento (--) disminuye (o resta de a uno) su operando y retorna un valor. https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/Decrement
-        if(timeLeft.m >= 0 && timeLeft.s < 0){
-            timeLeft.s = 59;
-            --timeLeft.m;
-            if(timeLeft.h >= 0 && timeLeft.m < 0){
-                timeLeft.m = 59;
-                --timeLeft.h;
-                if(timeLeft.d >= 0 && timeLeft.h < 0){
-                    timeLeft.h = 23;
-                    --timeLeft.d;
-                };
-            };
-        };
-    };
-    --totalSeconds;
-    printTime();
-};
-function printTime() {
-    days.innerText = timeLeft.d;
-    hours.innerText = timeLeft.h;
-    minutes.innerText = timeLeft.m;
-    seconds.innerText = timeLeft.s;
-};
-function setTimeLeft() {
-    timeLeft.d = Math.round(totalSeconds / (60 * 60 * 24));
-    timeLeft.h = Math.round(totalSeconds / (60 * 60) % 24);
-    timeLeft.m = Math.round(totalSeconds / (60) % 60);
-    timeLeft.s = Math.round(totalSeconds % 60);
-}
--- JS/index.js
+++ JS/index.js
@@ -2,13 +2,13 @@ import menuHamburguesa from "./menu_hamburguesa.js";
 import { digitalClock } from "./digital_clock.js";
 import musicBotton from "./sound_botton.js";
 import moveBall from "./keyboard_events.js";
-import init from "./countdown.js";
+import countdown from "./countdown.js";
 const d = document;
 d.addEventListener("DOMContentLoaded", (e) => {
     menuHamburguesa();
     digitalClock("#realTimeClock", "#activateWatch", "#deactivateWatch");
     musicBotton("#activateMusic", "#deactivateMusic");
-    init();
+    countdown("Jul 26 2021 21:52:00");
 });
 d.addEventListener("keydown", (e) => {
     moveBall(e, ".section2-stage", ".section2-ball");
