aa4da16023b9fd58fc22648ed47fb2c62ad19b83
David Avellaneda <davidavellanedagit@gmail.com>
2021-07-28 14:49:27 -0500

    Efecto hover al botón scroll top y JS  a scroll_top modificado

--- JS/index.js
+++ JS/index.js
@@ -3,18 +3,16 @@ import { digitalClock } from "./digital_clock.js";
 import musicBotton from "./sound_botton.js";
 import moveBall from "./keyboard_events.js";
 import countdown from "./countdown.js";
-import {scrollTop, goToStart} from "./scroll_top.js";
+import scrollTop from "./scroll_top.js";
 const d = document;
 d.addEventListener("DOMContentLoaded", () => {
     menuHamburguesa();
     digitalClock("#realTimeClock", "#activateWatch", "#deactivateWatch");
     musicBotton("#activateMusic", "#deactivateMusic");
     countdown("Dec 24 2021 24:00:00");
+    scrollTop(".scrollTop");
 });
 const $section2_stage = document.getElementById("section2-stage");
 $section2_stage.addEventListener("click", () => {
     d.addEventListener("keydown", (e) => moveBall(e, ".section2-stage", ".section2-ball"));
 });
-d.addEventListener("scroll", () => scrollTop());
-const $scrollToTop = document.getElementById("scrollToTop")
-$scrollToTop.addEventListener("click", () => goToStart("scrollToTop"));
--- JS/scroll_top.js
+++ JS/scroll_top.js
@@ -1,18 +1,19 @@
-const $button_scrollTop = document.querySelector(".scrollTop");
-export function scrollTop() {
-    if(window.scrollY > 600) {
-        $button_scrollTop.classList.toggle("active", window.scrollY > 600);
-        //$buttom_scrollTop.classList.add("active");
-    }
-    if(window.scrollY < 600){
-        $button_scrollTop.classList.remove("active");
-    }
-}
-export function goToStart(id) {
-    document.getElementById(id);
-    scrollTo({ // window.scrollTo también se puede escribir así
-        top: 0
-        // El ID es el botón que aparece cuando se hace scroll, entonces "scrollTo" lo lleva al inicio por eso top: 0
-        // window.scrollTo Desplaza el visor a un conjunto específico de coordenadas en el documento, es este caso hacia top en 0
-    });
+export default function scrollTop(btn) {
+        const $button_scrollTop = document.querySelector(btn);
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 600) {
+                $button_scrollTop.classList.add("active");
+                //$buttom_scrollTop.classList.toggle("active");
+            } else {
+                $button_scrollTop.classList.remove("active");
+            };
+        });
+        $button_scrollTop.addEventListener("click", () => 
+            scrollTo({ // window.scrollTo también se puede escribir así
+                top: 0,
+                behavior: "smooth"
+                // El ID es el botón que aparece cuando se hace scroll, entonces "scrollTo" lo lleva al inicio por eso top: 0
+                // window.scrollTo Desplaza el visor a un conjunto específico de coordenadas en el documento, es este caso hacia top en 0
+            })
+        );
 };
