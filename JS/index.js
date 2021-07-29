e29752c0aa78b5f6f39f790776aca048603dc60d
David Avellaneda <davidavellanedagit@gmail.com>
2021-07-28 22:47:21 -0500

    Duración de scroll al presionar un link del menú añadida

--- JS/index.js
+++ JS/index.js
@@ -5,6 +5,7 @@ import moveBall from "./keyboard_events.js";
 import countdown from "./countdown.js";
 import scrollTop from "./scroll_top.js";
 import { darkMode } from "./dark_mode.js";
+import smoothScroll from "./smooth_link_scrolling.js"
 const d = document;
 d.addEventListener("DOMContentLoaded", () => {
     menuHamburguesa();
@@ -13,6 +14,28 @@ d.addEventListener("DOMContentLoaded", () => {
     countdown("Dec 24 2021 24:00:00");
     scrollTop(".scrollTop");
     darkMode("changeBackground");
+
+    const links = document.querySelectorAll(".menu-link");
+    const firstLink = links[0];
+    firstLink.addEventListener("click", () => {
+        smoothScroll("#reloj-digital-y-alarma", "3000");
+    });
+    const secondLink = links[1];
+    secondLink.addEventListener("click", () => {
+        smoothScroll("#eventos-del-teclado", "3000");
+    });
+    const thirdLink = links[2];
+    thirdLink.addEventListener("click", () => {
+        smoothScroll("#cuenta-regresiva", "3000");
+    });
+    const fourthLink = links[3];
+    fourthLink.addEventListener("click", () => {
+        smoothScroll("#section4", "3000");
+    });
+    const fifthLink = links[4];
+    fifthLink.addEventListener("click", () => {
+        smoothScroll("#section5", "3000");
+    });
 });
 const $section2_stage = document.getElementById("section2-stage");
 $section2_stage.addEventListener("click", () => {
---
+++ JS/smooth_link_scrolling.js
@@ -0,0 +1,43 @@
+// window.onload = () => {
+//     const links = document.querySelectorAll(".menu-link");
+//     const firstLink = links[0];
+//     firstLink.addEventListener("click", () => {
+//         smoothScroll("#reloj-digital-y-alarma", "3000");
+//     });
+//     const secondLink = links[1];
+//     secondLink.addEventListener("click", () => {
+//         smoothScroll("#eventos-del-teclado", "3000");
+//     });
+//     const thirdLink = links[2];
+//     thirdLink.addEventListener("click", () => {
+//         smoothScroll("#cuenta-regresiva", "3000");
+//     });
+//     const fourthLink = links[3];
+//     fourthLink.addEventListener("click", () => {
+//         smoothScroll("#section4", "3000");
+//     });
+//     const fifthLink = links[4];
+//     fifthLink.addEventListener("click", () => {
+//         smoothScroll("#section5", "3000");
+//     });
+// }
+export default function smoothScroll(objective, duration) {
+    let elementObjective = document.querySelector(objective),
+        elementPosition = elementObjective.getBoundingClientRect().top, // devuelve el tamaño de un elemento y su posición relativa respecto a la ventana de visualización es este cuánto está separado del top
+        initialPosition = window.pageYOffset, // devuelve el número de píxeles que el documento se desplaza actualmente a lo largo del eje vertical
+        initialTime = null;
+    const animation = timeNow => {
+        if(initialTime === null) initialTime = timeNow;
+        let timeElapsed = timeNow - initialTime,
+            auxiliaryAnimation = equationForAnimation(timeElapsed, initialPosition, elementPosition, duration);
+        window.scrollTo(0, auxiliaryAnimation); // 0 en Y y en X auxiliaryAnimation
+        if(timeElapsed < duration) requestAnimationFrame(animation);
+    };
+    requestAnimationFrame(animation);
+    const equationForAnimation = (t, b, c, d) => {
+        t /= d/2;
+        if (t < 1) return c/2*t*t*t*t*t + b;
+        t -= 2;
+        return c/2*(t*t*t*t*t + 2) + b;
+    };
+};
