e2b5e07d3effc21f5ed9c4660899d0480674653a
David Avellaneda <davidavellanedagit@gmail.com>
2021-07-29 11:43:51 -0500

    Click en el link y desaperece menú

--- JS/menu_hamburguesa.js
+++ JS/menu_hamburguesa.js
@@ -1,9 +1,22 @@
 export default function menuHamburguesa () {
     const btnMenu = document.getElementById("btnMenu"),
-    menu = document.getElementById("menu");
+        menu = document.getElementById("menu"),
+        menuLink = document.querySelectorAll(".menu-link");
     btnMenu.addEventListener("click", () => {
         menu.classList.toggle("mostrar");
-        menu.addEventListener("click", () => {
+        menuLink[0].addEventListener("click", () => {
+            menu.classList.remove("mostrar");
+        });
+        menuLink[1].addEventListener("click", () => {
+            menu.classList.remove("mostrar");
+        });
+        menuLink[2].addEventListener("click", () => {
+            menu.classList.remove("mostrar");
+        });
+        menuLink[3].addEventListener("click", () => {
+            menu.classList.remove("mostrar");
+        });
+        menuLink[4].addEventListener("click", () => {
             menu.classList.remove("mostrar");
         });
     });
