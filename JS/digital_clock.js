13572c97f1857e5047f6db3840f1d51dfadf5ad0
David Avellaneda <davidavellanedagit@gmail.com>
2021-08-18 22:38:56 -0500

    canceladas las interacciones cuando está el skeleton

--- JS/digital_clock.js
+++ JS/digital_clock.js
@@ -6,9 +6,12 @@ export function digitalClock (clock, activateWatch, deactivateWatch) {
     let realTimeClock;
     $activateWatch.addEventListener("click", (e) => {
         realTimeClock = setInterval(() => {
-            let hours = new Date().getHours();
-            let minutes = new Date().getMinutes();
-            let seconds = new Date().getSeconds();
+            let hours = new Date().getHours(),
+                minutes = new Date().getMinutes(),
+                seconds = new Date().getSeconds();
+            hours = ("0" + hours).slice(-2);
+            minutes = ("0" + minutes).slice(-2);
+            seconds = ("0" + seconds).slice(-2);
             const format = hours >= 12 ? "PM" : "AM"; // Si el diviendo es menor al divisor 5 / 7 el módulo será el dividendo "5", y si el diviendo es mayor el módulo será el residuo
             $clock.innerHTML = `<h3 class="realTimeClock-title">${hours % 12}:${minutes}:${seconds} ${format}</h3>`;
             divClock.style.setProperty("width","100%");
