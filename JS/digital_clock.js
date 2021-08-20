9148dc63bd6abef36141880c4c4a0ba892d90464
David Avellaneda <davidavellanedagit@gmail.com>
2021-08-20 12:49:14 -0500

    animación al reloj y webcam modificada

--- JS/digital_clock.js
+++ JS/digital_clock.js
@@ -4,7 +4,12 @@ export function digitalClock (clock, activateWatch, deactivateWatch) {
         $activateWatch = document.getElementById(activateWatch),
         $deactivateWatch = document.getElementById(deactivateWatch);
     let realTimeClock;
+    $clock.innerHTML = null;
     $activateWatch.addEventListener("click", (e) => {
+        divClock.style.setProperty("transform", "translateX(-100%)");
+        runClock();
+    });
+    function runClock() {
         realTimeClock = setInterval(() => {
             let hours = new Date().getHours(),
                 minutes = new Date().getMinutes(),
@@ -13,20 +18,21 @@ export function digitalClock (clock, activateWatch, deactivateWatch) {
             minutes = ("0" + minutes).slice(-2);
             seconds = ("0" + seconds).slice(-2);
             const format = hours >= 12 ? "PM" : "AM"; // Si el diviendo es menor al divisor 5 / 7 el módulo será el dividendo "5", y si el diviendo es mayor el módulo será el residuo
-            $clock.innerHTML = `<h3 class="realTimeClock-title">${hours % 12}:${minutes}:${seconds} ${format}</h3>`;
+            // Operador ternariosin declarar variables
+            hours % 12 === 0 
+                ? $clock.innerHTML = `<h3 class="realTimeClock-title">${hours}:${minutes}:${seconds} ${format}</h3>`
+                : $clock.innerHTML = `<h3 class="realTimeClock-title">${hours % 12}:${minutes}:${seconds} ${format}</h3>`;
             divClock.style.setProperty("width","100%");
-            divClock.style.setProperty("margin-block-start","3rem");
-            divClock.style.setProperty("display","flex");
-            divClock.style.setProperty("justify-content","center");
-            divClock.style.setProperty("font-size","2rem");
-            divClock.style.setProperty("transition", "all .3s ease-in");
+            divClock.style.setProperty("height","3rem");
+            divClock.style.setProperty("transform", "translateX(0%)")
         }, 1000);
         // e.target.disabled = true; // Para que deshabilite el boton
         $deactivateWatch.addEventListener("click", () => {
             clearInterval(realTimeClock);
-            $clock.innerHTML = null;
-            divClock.style.setProperty("margin-block-start","0rem");
+            divClock.style.setProperty("height","0");
+            divClock.style.setProperty("transform", "translateX(-100%)");
+            setTimeout(() => {$clock.innerHTML = null;}, 1200);
             // $activateWatch.disabled = false;
         });
-    });
+    }
 };
--- JS/webcam.js
+++ JS/webcam.js
@@ -1,5 +1,3 @@
-// https://programmerclick.com/article/5793808195/
-const n_md = navigator.mediaDevices;
 function webcam(){
     const $containerTrue = document.getElementById("container-true"),
         $containerFalse = document.getElementById("container-false"),
@@ -27,60 +25,7 @@ function webcam(){
                 $containerTrue.style.width = "0";
                 $containerTrue.style.height = "0";
                 $containerTrue.style.overflow = "hidden";
-            }) // 
-        // if(n_md.getUserMedia){
-        //     if(typeof currentStream !== "undefined") stopMediaTracks(currentStream);
-        // const videoLimitations = {};
-        // if($select.value === "") videoLimitations.facingMode = "environment"
-        // else videoLimitations.deviceId = {exact: $select.value}
-        // const limitations = {video:true, audio:false};
-        // n_md.getUserMedia(limitations).then(stream => {currentStream = stream; $video.srcObject = stream; return n_md.enumerateDevices();}).then(gotDevices).catch(error => {
-        //     $containerVideo.style.width = "0";
-        //     $containerVideo.style.height = "auto";
-        //     $containerVideo.style.marginBlockEnd = "0";
-        //     $containerVideo.style.transform = "translateX(-100%)";
-        // });
-        // function gotDevices(mediaDevices){
-        //     $select.innerHTML = "";
-        //     let count = 1;          
-        //     mediaDevices.forEach(mediaDevice => {
-        //         if (mediaDevice.kind === 'videoinput') {
-        //             const $camera = document.createElement('div');
-        //                 option = document.createElement('h4');
-        //             $containerVideo.style.width = "100%";
-        //             $containerVideo.style.height = "auto";
-        //             $containerVideo.style.marginBlockEnd = "10rem";
-        //             $containerVideo.style.transform = "translateX(0%)";
-        //             $camera.classList.add('camera')
-        //             $camera.style.width = "2rem";
-        //             $camera.style.display = "flex";
-        //             $camera.innerHTML = `<svg id="pictureCamera" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"viewBox="0 0 512 426" style="enable-background:new 0 0 512 426; fill:#0bb92b;" xml:space="preserve">
-        //                                     <g transform="translate(0.000000,512.000000) scale(0.100000,-0.100000)">
-        //                                         <path d="M1615,5041c-115-37-225-123-281-218c-53-90-66-148-71-305l-5-148H873c-428,0-453-3-574-66S73,4115,27,3981l-22-66L2,2670
-        //                                             c-2-875,1-1264,8-1310c32-187,171-353,356-427l69-28h2125h2125l69,28c185,74,324,240,356,427c7,46,10,435,8,1310l-3,1245l-22,66
-        //                                             c-46,134-151,260-272,323s-146,66-574,66h-385l-5,148c-5,157-18,215-71,305c-57,97-168,182-286,219c-51,16-127,18-945,17
-        //                                             C1757,5058,1660,5056,1615,5041z M2776,3865c858-150,1302-1120,856-1870c-333-561-1055-769-1636-472c-369,188-609,523-671,937
-        //                                             c-68,444,126,904,496,1180c143,107,339,192,514,223C2445,3883,2666,3884,2776,3865z"/>
-        //                                         <path d="M2445,3454c-379-61-667-363-704-736c-39-404,210-766,607-880c110-32,314-32,424,0c335,96,573,374,606,707
-        //                                             c25,258-57,493-237,672c-99,99-239,180-376,217C2705,3451,2502,3463,2445,3454z"/>
-        //                                     </g>
-        //                                 </svg>`
-        //             $select.appendChild($camera);
-        //             $select.style.justifyContent = 'space-around';
-        //             $select.style.alignItems = 'center';
-        //             option.value = mediaDevice.deviceId;
-        //             const label = mediaDevice.label || `Camera ${count++}`;
-        //             const textNode = document.createTextNode(label);
-        //             option.appendChild(textNode);
-        //             $select.appendChild(option);
-        //         }
-        //     });
-        // };
-        // function stopMediaTracks(stream) {
-        //     stream.getTracks().forEach(track => track.stop());
-        // };
-        // n_md.enumerateDevices().then(gotDevices)
-        // }
+            });
     });
 };
 webcam()
