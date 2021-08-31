6f2c90e86f142cfd18c5177c4ec75baea2986b30
David Avellaneda <davidavellanedagit@gmail.com>
2021-08-30 19:03:35 -0500

    Imágenes de slider optimizadas

--- JS/index.js
+++ JS/index.js
@@ -48,20 +48,20 @@ d.addEventListener("DOMContentLoaded", () => {
         "(min-width:850px)", 
         `<h4 class="youtube-title-mobile">Da click en la imagen para ver el video</h4>
         <a href="https://youtu.be/P982oehprfY" target="_blank" rel="noreferrer noopener" style="grid-column:1/3; grid-row:2;">
-            <img src="https://i.ytimg.com/vi_webp/P982oehprfY/maxresdefault.webp" style="width:100%;" />
+            <img loading="lazy" src="https://i.ytimg.com/vi_webp/P982oehprfY/maxresdefault.webp" style="width:100%;" />
         </a>`, // mobileContent, utilizamos las template strings
         `<h4 class="youtube-title-desktop">Da click para ver el video</h4>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/P982oehprfY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="width:100%; grid-column:1/3;"></iframe>` // desktopContent, utilizamos las template strings
+        <iframe loading="lazy" width="560" height="315" src="https://www.youtube.com/embed/P982oehprfY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="width:100%; grid-column:1/3;"></iframe>` // desktopContent, utilizamos las template strings
     ); 
     responsive_object(
         "maps", // ID maps
         "(min-width:850px)", 
         `<h4 class="maps-title-mobile">Da click en la imagen para ver la ubicación</h4>
         <a href="https://goo.gl/maps/oBccT9j6u9uhhakA9" target="_blank" rel="noreferrer noopener" style="grid-column:1/3; grid-row:2; display:flex; justify-content:center;">
-            <img src="./assets/Icons/mapa.png" style="width:70%;" />
+            <img loading="lazy" src="./assets/Icons/mapa.png" style="width:70%;" />
         </a>`, // mobileContent, utilizamos las template strings
         `<h4 class="maps-title-desktop">Da click para ver el maps</h4>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3956.263701817781!2d-74.0782321852529!3d4.598120596659561!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f99a7eccfe58f%3A0x99cb72b35351476!2sPlaza%20de%20Bol%C3%ADvar%20de%20Bogot%C3%A1!5e1!3m2!1ses!2sco!4v1627661077028!5m2!1ses!2sco" width="600" height="450" style="border:0; width:100%; height: 315px; grid-column:1/3;" allowfullscreen="" loading="lazy"></iframe>` // desktopContent, utilizamos las template strings
+        <iframe loading="lazy" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3956.263701817781!2d-74.0782321852529!3d4.598120596659561!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8e3f99a7eccfe58f%3A0x99cb72b35351476!2sPlaza%20de%20Bol%C3%ADvar%20de%20Bogot%C3%A1!5e1!3m2!1ses!2sco!4v1627661077028!5m2!1ses!2sco" width="600" height="450" style="border:0; width:100%; height: 315px; grid-column:1/3;" allowfullscreen="" loading="lazy"></iframe>` // desktopContent, utilizamos las template strings
     );
     userAgent("operating-system-name");
     setTimeout(() => {
--- JS/slider.js
+++ JS/slider.js
@@ -5,25 +5,36 @@ export default function slider(){
     let $sliderSection = document.querySelectorAll(".slider-section"),
         $sliderSectionLast = $sliderSection[$sliderSection.length -1]; // De esta manera se obtiene al último section
     $slider.insertAdjacentElement('afterbegin', $sliderSectionLast); // Lo coloca después de empezar, es decir de primeras
-    $rightBtn.addEventListener('click', () => {
+    function moveRight(){
         let $sliderSectionFirst = document.querySelectorAll(".slider-section")[0]; // Toma al primero
         $slider.style.marginInlineStart = "-200%";
-        $slider.style.transition = "all 1s ease"
+        $slider.style.transition = "all 1.6s ease"
         setTimeout(() => {
             $slider.style.transition = "none";
             $slider.insertAdjacentElement('beforeend', $sliderSectionFirst); // Beforeend es "ANTES DE TERMINAR", es decir antes de que finalice la divi donde esta $slider
             $slider.style.marginInlineStart = "-100%";
-        }, 900);
-    });
+        }, 1500);
+    }
     $leftBtn.addEventListener('click', () => {
-        let $sliderSectionFirst = document.querySelectorAll(".slider-section")[2]; // Toma el último
-        $slider.style.marginInlineStart = "0%";
-        $slider.style.transition = "all 1s ease"
+        let $sliderSection = document.querySelectorAll(".slider-section"),
+            $sliderSectionFirst = $sliderSection[$sliderSection.length -1];
+        $slider.style.marginInlineStart = "0";
+        $slider.style.transition = "all 1.6s ease"
         setTimeout(() => {
             $slider.style.transition = "none";
             $slider.insertAdjacentElement('afterbegin', $sliderSectionFirst); // Beforeend es "DESPUÉS DE EMPEZAR", es decir antes de que la primera imagen en la div = $slider
             $slider.style.marginInlineStart = "-100%";
-        }, 900);
+        }, 1500);
+        clearInterval(currentTime); // Cuando le de click al botón de la izquierda detenemos el setInterval
+        currentTime = setInterval(() => moveRight(), 10000); // Volvemos a iniciarlo
     });
+    $rightBtn.addEventListener('click', () => {
+        moveRight();
+        clearInterval(currentTime); // Cuando le de click detenemos el setInterval
+        currentTime = setInterval(() => moveRight(), 10000); // Aquí cuando le de click y sea una nueva imagen que vuelva a reiniciar el setInterval, por eso lo detenemos y lo volvemos a iniciar para que inicie desde 0 segundos
+    });
+    let currentTime =
+        setInterval(() => {
+            moveRight();
+        }, 10000);
 };
-
